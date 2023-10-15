986d33576c182a23471994da322469b145317f31
abdurahmanaied <abdurahmanaied@gmail.com>
2023-10-15 21:29:11 +0300

    complate admin/project and home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,122 @@
 <template>
-  <div class="row gy-4">
+  <div class="row gy-4" id="div_parentNode">
     <div class="col-lg-4 col-md-12">
-      <a href="/1" class="text-decoration-none">
-        <div class="card shadow">
-          <div class="card-header text-bg-primary bg-gradient">
-            Prject Id
-          </div>
-          <img src="https://placehold.co/100" alt="alt">
-          <div class="card-body">
-            <h5>Prject Title</h5>
-            <p class="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi aliquam nam nobis voluptas adipisci natus quae
-              amet explicabo ullam rerum quod odio accusantium ea, accusamus, iure consectetur eius tenetur porro!
-            </p>
-          </div>
+      <div class="card shadow">
+        <div class="card-header placeholder-glow">
+          <span class="placeholder col-6"></span>
         </div>
-      </a>
+        
+        <svg class="bd-placeholder-img" width="100%" height="auto" style="aspect-ratio: 1/1;" xmlns="http://www.w3.org/2000/svg">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+        </svg>
+
+        <div class="card-body placeholder-glow">
+          <span class="placeholder col-7"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-6"></span>
+          <span class="placeholder col-8"></span>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-12">
+      <div class="card shadow">
+        <div class="card-header placeholder-glow">
+          <span class="placeholder col-6"></span>
+        </div>
+        
+        <svg class="bd-placeholder-img" width="100%" height="auto" style="aspect-ratio: 1/1;" xmlns="http://www.w3.org/2000/svg">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+        </svg>
+
+        <div class="card-body placeholder-glow">
+          <span class="placeholder col-7"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-6"></span>
+          <span class="placeholder col-8"></span>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-12">
+      <div class="card shadow">
+        <div class="card-header placeholder-glow">
+          <span class="placeholder col-6"></span>
+        </div>
+        
+        <svg class="bd-placeholder-img" width="100%" height="auto" style="aspect-ratio: 1/1;" xmlns="http://www.w3.org/2000/svg">
+          <title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#868e96"></rect>
+        </svg>
+
+        <div class="card-body placeholder-glow">
+          <span class="placeholder col-7"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-4"></span>
+          <span class="placeholder col-6"></span>
+          <span class="placeholder col-8"></span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { db } from "@/plugins/Firebase"
+import { collection, getDocs, updateDoc, doc, getDoc, addDoc, deleteDoc } from 'firebase/firestore';
+
+async function loadProjects(parentNode, projectsCollectionRef) {
+  const placeHolderHtml = `
+      <div class="col-lg-4 col-md-12">
+        <a href="/project?id=%project_id%" class="text-decoration-none">
+          <div class="card shadow">
+            <div class="card-header text-bg-primary bg-gradient">
+              %project_name%
+            </div>
+            <img src="https://placehold.co/100" alt="alt" width="100%" height="auto" style="aspect-ratio: 1/1;">
+            <div class="card-body">
+              <p class="card-text">
+                %project_description%
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    `
+
+  //
+  const projectsSnapshot = await getDocs(projectsCollectionRef)
+
+  let child = parentNode.firstElementChild
+  while (child) {
+    child.remove()
+    child = parentNode.firstElementChild
+  }
+
+  for (let doc of projectsSnapshot.docs) {
+    const project = doc.data()
+
+    let html = placeHolderHtml.replace("%project_name%", project.name)
+    html = html.replace("%project_description%", project.description)
+
+    html = html.replaceAll("%project_id%", doc.id)
+
+    parentNode.insertAdjacentHTML("afterbegin", html)
+  }
+}
+
 export default {
-  mounted() {
+  async mounted() {
     window.loading(true)
-    //
-    // window.loading(false)
+
+    const parentNode = document.querySelector("#div_parentNode")
+    const projectsCollectionRef = collection(db, "projects")
+
+    await loadProjects(parentNode, projectsCollectionRef)
+
+    window.loading(false)
   }
 }
 </script>
@@ -33,5 +124,4 @@ export default {
 <style>
 .card:hover .card-body {
   background-color: var(--bs-tertiary-bg) !important;
-}
-</style>
+}</style>
--- src/views/admin/Project.vue
+++ src/views/admin/Project.vue
@@ -5,30 +5,30 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-3">
-                        <label for="input_projectName" class="form-label">Project Name</label>
-                        <input type="text" id="input_projectName" class="form-control">
+                        <label for="name" class="form-label">Project Name</label>
+                        <input type="text" id="name" class="form-control" required>
                     </div>
                     <div class="col-3">
-                        <label for="input_companyName" class="form-label">Company</label>
-                        <input type="text" id="input_companyName" class="form-control">
+                        <label for="company" class="form-label">Company</label>
+                        <input type="text" id="company" class="form-control" required>
                     </div>
                     <div class="col-3">
-                        <label for="input_durationInMonths" class="form-label">Duration (months)</label>
+                        <label for="duration" class="form-label">Duration (months)</label>
                         <div class="input-group">
-                            <input type="number" id="input_durationInMonths" class="form-control">
+                            <input type="number" id="duration" class="form-control" required>
                             <span class="input-group-text">Months</span>
                         </div>
                     </div>
                     <div class="col-3">
-                        <label for="input_totalCost" class="form-label">Total Cost</label>
+                        <label for="cost" class="form-label">Total Cost</label>
                         <div class="input-group">
-                            <input type="number" id="input_totalCost" class="form-control">
+                            <input type="number" id="cost" class="form-control" required>
                             <span class="input-group-text">SR</span>
                         </div>
                     </div>
                     <div class="col-12 mt-2">
-                        <label for="input_projectText" class="form-label">Project description</label>
-                        <textarea name="input_projectText" id="input_projectText" rows="10" class="form-control"></textarea>
+                        <label for="description" class="form-label">Project description</label>
+                        <textarea name="description" id="description" rows="10" class="form-control"></textarea>
                     </div>
                 </div>
 
@@ -43,7 +43,7 @@
 
                 <table class="table table-striped mt-3">
                     <tbody>
-                        <tr v-for="n in 4" :key="'table1-option-col' + n">
+                        <tr>
                             <td>[document_name]</td>
                             <td class="text-end">
                                 <button class="btn btn-sm btn-primary">Preview</button>
@@ -53,24 +53,102 @@
                     </tbody>
                 </table>
             </div>
-            
+
             <div class="card-body">
-                <div class="alert alert-danger d-flex align-items-center">
-                    <i class="fas fa-triangle-exclamation fs-4 me-3"></i>
-                    <span>Please fill all the reqired fields</span>
-                </div>
-                <div class="alert alert-success d-flex align-items-center">
-                    <i class="fas fa-check fs-4 me-3"></i>
-                    <span>The changes has been saved successfuly</span>
+                <div class="alert alert-info d-flex align-items-center d-none">
+                    <i class="fas fa-info fs-4 me-3"></i>
+                    <span>Alert content goes here</span>
                 </div>
             </div>
 
             <div class="card-footer d-flex">
-                <button class="btn btn-secondary">
+                <a href="/admin/projects" class="btn btn-secondary">
                     <i class="fas fa-angle-left me-2"></i>Back
-                </button>
+                </a>
                 <input type="submit" value="Save Changes" class="btn btn-success ms-auto">
             </div>
         </form>
     </div>
 </template>
+
+<script>
+import { db } from "@/plugins/Firebase"
+import { collection, getDocs, updateDoc, doc, getDoc, addDoc, deleteDoc } from 'firebase/firestore';
+import { FirebaseError } from "firebase/app";
+
+function loadProject(form, project) {
+    form.name.value = project.name
+    form.company.value = project.company
+    form.duration.value = project.duration
+    form.cost.value = project.cost
+    form.description.value = project.description ? project.description : ""
+}
+
+function DisplayAlert(type, message) {
+    const div_alert = document.querySelector(".alert")
+
+    div_alert.className = `alert alert-${type} d-flex align-items-center`
+    div_alert.querySelector("span").textContent = message
+
+    let icon
+    switch (type) {
+        case "success":
+            icon = "check"
+            break
+        case "danger":
+            icon = "triangle-exclamation"
+            break
+        default:
+            icon = "info"
+    }
+
+    div_alert.querySelector("i").className = `fas fa-${icon} fs-4 me-3`
+}
+
+export default {
+    async mounted() {
+        window.loading(true)
+
+        const urlParams = new URLSearchParams(location.search)
+        const projectId = urlParams.get("id")
+
+        const projectDocRef = doc(db, `projects/${projectId}`)
+        const projectSnapshot = await getDoc(projectDocRef)
+        const project = projectSnapshot.data()
+
+        document.querySelector("h5").textContent = `Manage ${project.name}`
+
+        const form = document.querySelector("form")
+
+        loadProject(form, project)
+
+        form.addEventListener("submit", async e => {
+            e.preventDefault()
+
+            window.loading(true)
+            try {
+                await updateDoc(projectDocRef, {
+                    name: form.name.value,
+                    company: form.company.value,
+                    duration: Number(form.duration.value),
+                    cost: Number(form.cost.value),
+                    description: form.description.value,
+                })
+
+                DisplayAlert("success", "The changes has been saved successfuly.")
+            } catch (error) {
+                if (!(error instanceof FirebaseError)) {
+                    DisplayAlert("danger", "An error has been occured in the system. Please try again later.")
+                    throw error
+                }
+                console.error(error);
+                DisplayAlert("danger", "An error has been occured while update the project. Please try again later.")
+            } finally {
+                window.loading(false)
+            }
+        })
+
+        window.loading(false)
+    }
+}
+</script>
